315d337c7c1a67adb26b2901cff79216cb77d557
ABHIRAM-DAFLAPURKAR <abhiramdm10@gmail.com>
2026-02-06 00:04:35 +0530

    Initial backend setup with Docker, Postgres and MongoDB

--- backend/lexora-backend/src/main/java/com/lexora/lexora_backend/LexoraBackendApplication.java
+++ backend/lexora-backend/src/main/java/com/lexora/lexora_backend/LexoraBackendApplication.java
@@ -3,6 +3,8 @@ package com.lexora.lexora_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//mvn spring-boot:run -DskipTests
+
 @SpringBootApplication
 public class LexoraBackendApplication {
 
--- backend/lexora-backend/src/test/java/com/lexora/lexora_backend/LexoraBackendApplicationTests.java
+++ backend/lexora-backend/src/test/java/com/lexora/lexora_backend/LexoraBackendApplicationTests.java
@@ -3,11 +3,14 @@ package com.lexora.lexora_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration,org.springframework.boot.mongodb.autoconfigure.MongoReactiveAutoConfiguration,org.springframework.boot.servlet.autoconfigure.MultipartAutoConfiguration,org.springframework.boot.mongodb.autoconfigure.health.MongoHealthContributorAutoConfiguration",
+    "management.health.mongo.enabled=false"
+})
 class LexoraBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // This test will pass if the Spring context loads successfully
+    }
 }
